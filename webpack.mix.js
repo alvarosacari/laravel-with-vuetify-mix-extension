d8f2893a0c703b1abe913d72675edeb805219e99
Alvaro <alvaro.sacari@gmail.com>
2021-06-04 13:05:49 -0500

    feat: update options, webpackConfig, alias and version configurations

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,11 @@ const mix = require('laravel-mix')
 const path = require('path')
 require('vuetifyjs-mix-extension')
 
+const RESOURCES_CONTEXT = 'resources/js'
+const JS_FOLDER = 'js_generated'
+const IMG_FOLDER = 'img_generated'
+const MEDIA_FOLDER = 'media_generated'
+
 if (Config.hmr) {
   Config.hmrOptions = {
     https: false,
@@ -13,13 +18,55 @@ if (Config.hmr) {
 }
 
 mix.alias({
-  '@': path.join(__dirname, 'resources/js')
+  '@': path.join(__dirname, RESOURCES_CONTEXT)
+})
+
+mix.options({
+  terser: {
+    extractComments: false
+  },
+  postCss: [require('autoprefixer')]
+})
+
+mix.webpackConfig({
+  module: {
+    rules: [
+      {
+        test: /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: `${IMG_FOLDER}/[path][name].[ext]?[hash]`,
+              context: RESOURCES_CONTEXT,
+              esModule: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(mp3|mp4)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: `${MEDIA_FOLDER}/[path][name].[ext]?[hash]`,
+              context: RESOURCES_CONTEXT,
+              esModule: false
+            }
+          }
+        ]
+      }
+    ]
+  }
 })
 
 mix
-  .js('resources/js/main.js', 'public/js_generated')
-  .vuetify('vuetify-loader', 'resources/js/plugins/vuetify/vuetify-variables.scss', {
-    progressiveImages: true,
-  })
-  .vue()
+  .js(`${RESOURCES_CONTEXT}/main.js`, `public/${JS_FOLDER}`)
+  .vuetify('vuetify-loader', `${RESOURCES_CONTEXT}/plugins/vuetify/vuetify-variables.scss`)
+  .vue({ version: 2 })
+  .extract(['vue', 'vuetify'])
 
+if (process.env.NODE_ENV !== 'development') {
+  mix.version()
+}
